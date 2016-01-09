e15db4adf3b8df212703e57cf2677cb64c834e47
Kosuke Otake <sga0221@gmail.com>
2016-01-10 00:33:33 +0900

    インデントの修正?(2)

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,22 +1,22 @@
 module.exports = function(robot) {
-	robot.hear(/calc ([0-9]+\s*)(\S)(\s*[0-9]+)/, function(msg) {
-		var num1 = parseInt(msg.match[1]); //一つ目の数
-		var num2 = parseInt(msg.match[3]); //二つ目の数
-		var op = msg.match[2]; //演算子
-		var ans;
-
-		if(op == "+"){
-			ans = num1 + num2;
-		}else if(op == "-"){
-			ans = num1 - num2;
-		}else if(op == "*"){
-			ans = num1 * num2;
-		}else if(op == "/"){
-			ans = num1 / num2;
-		}else{
-			ans = "invalid op";
-		}
-		
-		msg.send(ans);
-	});
+    robot.hear(/calc ([0-9]+\s*)(\S)(\s*[0-9]+)/, function(msg) {
+	var num1 = parseInt(msg.match[1]); //一つ目の数
+	var num2 = parseInt(msg.match[3]); //二つ目の数
+	var op = msg.match[2]; //演算子
+	var ans;
+	
+	if(op == "+"){
+	    ans = num1 + num2;
+	}else if(op == "-"){
+	    ans = num1 - num2;
+	}else if(op == "*"){
+	    ans = num1 * num2;
+	}else if(op == "/"){
+	    ans = num1 / num2;
+	}else{
+	    ans = "invalid op";
+	}
+	
+	msg.send(ans);
+    });
 };
