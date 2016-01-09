545fb8ed9bf3adaed74eb86ebf1d922510aa7cac
Kosuke Otake <sga0221@gmail.com>
2016-01-10 00:24:28 +0900

    インデントの修正

--- scripts/ilovejs.js
+++ scripts/ilovejs.js
@@ -1,5 +1,5 @@
 module.exports = function(robot) {
-    robot.respond(/I love/i, function(msg) {
-	msg.send('javascript!');
-    });
+	robot.respond(/I love/i, function(msg) {
+		msg.send('javascript!');
+	});
 };
